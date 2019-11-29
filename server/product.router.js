e60de13a9a01a0f2890f464162d3e321ab3114b0
Krispol <kristjan.poldmets@gmail.com>
2019-11-29 09:17:49 +0200

    varasemad asjad

--- server/product.router.js
+++ server/product.router.js
@@ -15,8 +15,17 @@ router.get("/random", async (req, res)=>{
 });
 
 router.get("/similar/:productId", async (req, res)=>{
-  res.status(500).send("Implement")
+  const id = req.params.productId;
+  const mongoId = new mongoose.Types.ObjectId(id)
+  const product = await Product.findOne({ id:mongoId});
+  const brand = product.title.split{" "}[0];
+  const similarItems = await Product.find({ "title" : { $regex: /brand/, $options: "i"}});
+  res.status(200).send(similarItems);
 });
+
+//https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Async_await
+
+//https://stackoverflow.com/questions/38497650/how-to-find-items-using-regex-in-mongoose
 
 module.exports = router;
 
